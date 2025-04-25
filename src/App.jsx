a90c7930c5880576fd0fad5c1b6ffee0dcb04fb9
Simon <152021717+SimonR-prog@users.noreply.github.com>
2025-04-25 12:06:13 +0200

    Moved the import of the styling to their components.

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,7 @@
 import { Suspense } from 'react'
 import './App.css'
 import './stylings/Event_card.css'
-import './stylings/Calendar_days_card.css'
-import './stylings/Paid_status_card.css'
-import './stylings/Footer.css'
+
 
 
 import RouteRenderer from './routing/RouteRenderer';
