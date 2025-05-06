e05715b9bf8187347ab07c75504fb6763f1e8d0f
Simon <152021717+SimonR-prog@users.noreply.github.com>
2025-05-06 19:08:22 +0200

    Schedule - Admin and User side done.

---
+++ src/partials/pages/user/EVoucher.jsx
@@ -0,0 +1,45 @@
+import React from 'react'
+import '../../../stylings/Evoucher.css'
+import EventSchedule from '../../components/EventSchedule'
+
+
+
+
+const EVoucher = ({eventId}) => {
+    
+
+
+
+        
+    return (
+        <>
+            <main className='evoucher-main'>
+
+                <div className='evoucher-ticket'>
+
+                </div>
+                
+                <div className='evoucher-left'>
+                    <div className='evoucher-schedule'>
+                        <EventSchedule id={eventId} />
+                    </div>
+                    <div className='evoucher-terms'>
+
+                    </div>
+                </div>
+
+                <div className='evoucher-right'>
+                    <div className='evoucher-venue-map'>
+
+                    </div>
+                    <div className='evoucher-rules'>
+
+                    </div>
+                </div>
+
+            </main>
+        </>
+    )
+}
+
+export default EVoucher
